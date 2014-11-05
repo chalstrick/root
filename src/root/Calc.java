ebf3f5a501fb3726dd3ef110fff6f6b8311884e1
Christian Halstrick <christian.halstrick@sap.com>
2014-11-05 09:24:40 +0100

    add times4

--- src/root/Calc.java
+++ src/root/Calc.java
@@ -1,8 +1,11 @@
 package root;
 
+import root.calculations.Times4;
+
 public class Calc {
 	public static void main(String[] args) {
 		System.out.println("Math:");
 		System.out.println("PI: "+Math.PI);
+		System.out.println("4*PI: "+Times4.times4(Math.PI));
 	}
 }
---
+++ src/root/calculations/Times4.java
@@ -0,0 +1,7 @@
+package root.calculations;
+
+public class Times4 {
+	public static double times4(double value) {
+		return value*4.0;
+	}
+}
